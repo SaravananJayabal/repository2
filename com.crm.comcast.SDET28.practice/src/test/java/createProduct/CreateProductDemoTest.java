2c049bfa074a06f004b497186ab3e380ea6ffd7d
navee <navee@DESKTOP-8MA3HS2>
2022-01-21 09:54:45 +0530

    second push to framework

---
+++ com.crm.comcast.SDET28.practice/src/test/java/createProduct/CreateProductDemoTest.java
@@ -0,0 +1,11 @@
+package createProduct;
+
+import org.testng.annotations.Test;
+
+public class CreateProductDemoTest {
+	@Test
+	public void createProductDemoTest() {
+		System.out.println("adding new test script to product module");
+	}
+
+}
